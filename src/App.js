0fcef7c6fd243e0d206a7e759bffad1352f2afde
Sarah <sarahdvasquezg@gmail.com>
2025-09-29 21:07:39 +0200

    api_key set

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import 'maplibre-gl/dist/maplibre-gl.css';
 import './App.css';
 
 function App() {
+
+  const api_key="gbarpIC0LFulru5JPzI6"; //Sarah's key
+
   return (
     <div className="App">
       <Navbar/>
@@ -16,7 +19,7 @@ function App() {
           zoom: 14
         }}
         style={{width: "100%", height: " calc(100vh - 77px)"}}
-        mapStyle="https://api.maptiler.com/maps/streets/style.json?key=YOUR_MAPTILER_API_KEY_HERE"
+        mapStyle={`https://api.maptiler.com/maps/streets/style.json?key=${api_key}`}
       >
         <NavigationControl position="top-left" />
       </Map>
